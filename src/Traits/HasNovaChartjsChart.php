a162519c4f7fdc6ec4f369a488bf58e8188f849a
Navneet Rai <navneetrai@gmail.com>
2019-05-23 01:29:46 +0530

    Trait for adding NovaChartjsChart to models

---
+++ src/Traits/HasNovaChartjsChart.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace KirschbaumDevelopment\NovaChartjs\Traits;
+
+use Illuminate\Database\Eloquent\Relations\MorphOne;
+use KirschbaumDevelopment\NovaChartjs\Models\NovaChartjsMetricValue;
+
+trait HasNovaChartjsChart
+{
+    /**
+     * Get the Chartable Model's metric values
+     *
+     * @return MorphOne
+     */
+    public function novaChartjsMetricValue(): MorphOne
+    {
+        return $this->morphOne(NovaChartjsMetricValue::class, 'chartable');
+    }
+
+    /**
+     * Should return settings for Nova Chart in prescribed format
+     *
+     * @return array
+     */
+    abstract public function getNovaChartjsSettings():array;
+}
