755da1df4b1b6c163d4857686de67e42f584cfb7
Navneet Rai <navneetrai@gmail.com>
2019-05-24 00:48:53 +0530

    Update NovaChartjs class to inject metadata from model

--- src/NovaChartjs.php
+++ src/NovaChartjs.php
@@ -12,4 +12,29 @@ class NovaChartjs extends Field
      * @var string
      */
     public $component = 'nova-chartjs';
+
+    /**
+     * NovaChartjs constructor.
+     * Extending parent constructor to inject MetaData from Model
+     *
+     * @param $name
+     * @param null $attribute
+     * @param callable|null $resolveCallback
+     */
+    public function __construct($name, $attribute = null, callable $resolveCallback = null)
+    {
+        parent::__construct($name, $attribute, $resolveCallback);
+
+        // Fetching Nova Resource Class
+        $novaResourceClass = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS)[2]['class'];
+
+        if ($novaResourceClass) {
+            $this->withMeta([
+                // Fetching Settings enforce by HasNovaChartjsChart Trait
+                'settings' => $novaResourceClass::$model::getNovaChartjsSettings(),
+                // Fetching Preferred Label from NovaResourceClass
+                'label' => $novaResourceClass::singularLabel(),
+            ]);
+        }
+    }
 }
