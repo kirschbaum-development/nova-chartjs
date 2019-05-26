025d5dc9843ee6809052784bcb75f5fee6ba344b
Navneet Rai <navneetrai@gmail.com>
2019-05-27 00:19:23 +0530

    Added MetricValue resource to represent NovaChartjsMetricValue model

---
+++ src/Nova/MetricValue.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace KirschbaumDevelopment\NovaChartjs\Nova;
+
+use App\Nova\Resource;
+use Illuminate\Http\Request;
+use Laravel\Nova\Fields\MorphTo;
+use KirschbaumDevelopment\NovaChartjs\NovaChartjs;
+use KirschbaumDevelopment\NovaChartjs\Models\NovaChartjsMetricValue;
+
+class MetricValue extends Resource
+{
+    /**
+     * The model the resource corresponds to.
+     *
+     * @var string
+     */
+    public static $model = NovaChartjsMetricValue::class;
+
+    /**
+     * Indicates if the resource should be displayed in the sidebar.
+     *
+     * @var bool
+     */
+    public static $displayInNavigation = false;
+
+    /**
+     * The single value that should be used to represent the resource when being displayed.
+     *
+     * @var string
+     */
+    public static $title = 'id';
+
+    /**
+     * Get the fields displayed by the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     *
+     * @return array
+     */
+    public function fields(Request $request)
+    {
+        return [
+            NovaChartjs::make($this->resource->chartable, 'Metric Values', 'metric_values')
+                ->rules('required', 'json'),
+
+            MorphTo::make('Chartable')->onlyOnIndex(),
+        ];
+    }
+}
