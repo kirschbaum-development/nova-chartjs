09cbc01a1d03810580e914d8c17bf976c558ae15
Navneet Rai <navneetrai@gmail.com>
2019-05-27 00:53:32 +0530

    Json decode form data to convert it into array for hydrating model

--- src/NovaChartjs.php
+++ src/NovaChartjs.php
@@ -4,6 +4,7 @@ namespace KirschbaumDevelopment\NovaChartjs;
 
 use Illuminate\Support\Str;
 use Laravel\Nova\Fields\Field;
+use Laravel\Nova\Http\Requests\NovaRequest;
 
 class NovaChartjs extends Field
 {
@@ -34,6 +35,25 @@ class NovaChartjs extends Field
                 'settings' => $chartableClass::getNovaChartjsSettings(),
                 'label' => Str::singular(Str::title(Str::snake(class_basename($chartableClass), ' '))),
             ]);
+        }
+    }
+
+    /**
+     * Hydrate the given attribute on the model based on the incoming request.
+     *
+     * @param  \Laravel\Nova\Http\Requests\NovaRequest  $request
+     * @param  string  $requestAttribute
+     * @param  object  $model
+     * @param  string  $attribute
+     *
+     * @return mixed
+     */
+    protected function fillAttributeFromRequest(NovaRequest $request, $requestAttribute, $model, $attribute)
+    {
+        if ($request->exists($requestAttribute)) {
+            $value = json_decode($request[$requestAttribute], true);
+
+            $model->{$attribute} = $this->isNullValue($value) ? null : $value;
         }
     }
 }
