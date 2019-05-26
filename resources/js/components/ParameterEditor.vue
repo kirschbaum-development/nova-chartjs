867e882eb2a156e091de66b10c28a91e2963a2fa
Navneet Rai <navneetrai@gmail.com>
2019-05-27 00:07:26 +0530

    Created and added Parameter Editor Component to Form Field

---
+++ resources/js/components/ParameterEditor.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <div class="nova-items-field-input-wrapper flex p-2" v-for="(parameter, index) in this.parameters" :key="index">
+            <div class="w-1/4"><h4 class="font-normal text-80">{{parameter}}</h4></div>
+            <div class="w-3/4">
+                <input
+                    v-model="value[parameter]"
+                    type="number"
+                    :placeholder="parameter"
+                    class="flex-1 form-control form-input form-input-bordered"
+                />
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            'value': Object,
+            'parameters': Array
+        },
+    }
+</script>
