7e0607aab36acd1f7733128f30d50d183f036033
Navneet Rai <navneetrai@gmail.com>
2019-05-29 00:18:46 +0530

    Create Index view for Chartable

--- resources/js/components/IndexField.vue
+++ resources/js/components/IndexField.vue
@@ -1,9 +1,38 @@
 <template>
-    <span>{{ field.value }}</span>
+    <span>
+        <chartjs-bar-chart
+            :height="60"
+            :width="300"
+            :data="valueDataset"
+        />
+    </span>
 </template>
 
 <script>
+import ChartjsBarChart from "./ChartjsBarChart";
+import colors from "../mixins/colors";
+
 export default {
+    components: {ChartjsBarChart},
+
+    mixins: [colors],
+
     props: ['resourceName', 'field'],
+
+    computed: {
+        valueDataset: function () {
+            let color = this.field.settings.indexColor || this.field.settings.color || this.getRandomColor();
+            let colors = Array(this.field.settings.parameters.length).fill(color);
+            return {
+                labels: this.field.settings.parameters,
+                datasets: [
+                    {
+                        backgroundColor: colors,
+                        'data': Object.values(this.field.value)
+                    }
+                 ]
+            }
+        },
+    }
 }
 </script>
