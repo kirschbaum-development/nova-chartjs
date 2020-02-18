6fed5d9f30897066dea4f88fe8ed8f47c9e76e45
Navneet Rai <navneet@kirschbaumdevelopment.com>
2020-02-18 07:51:35 -0500

    Create Chartjs Radar Chart

---
+++ resources/js/components/ChartjsRadarChart.vue
@@ -0,0 +1,12 @@
+<script>
+    import { Radar, mixins } from 'vue-chartjs'
+    import charts from "../mixins/charts";
+
+    const {reactiveData} = mixins;
+
+    export default {
+        extends: Radar,
+
+        mixins: [reactiveData, charts],
+    }
+</script>
