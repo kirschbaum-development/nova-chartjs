1bffe36be7c12520158cddd1f59d0beb70512bf6
Navneet Rai <navneet@kirschbaumdevelopment.com>
2020-02-18 07:51:07 -0500

    Remove common code from existing charts

--- resources/js/components/ChartjsBarChart.vue
+++ resources/js/components/ChartjsBarChart.vue
@@ -8,24 +8,5 @@
         extends: Bar,
 
         mixins: [reactiveData, charts],
-
-        props: {
-            'dataset': Array,
-            'additionalDatasets': Array,
-            'settings': Object,
-        },
-
-        mounted () {
-            this.chartData = this.createChartDataset();
-            //we need to set options manually as options are used to re-render chart when data changes in reactiveData/reactiveProp mixin
-            this.options = this.settings.options;
-            this.renderChart(this.chartData, this.options);
-        },
-
-        watch: {
-            dataset: function() {
-                this.chartData = this.createChartDataset();
-            }
-        }
     }
 </script>
