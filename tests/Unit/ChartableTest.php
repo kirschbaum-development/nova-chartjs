0a984cf186da1d0826225707047d82425e4442e8
Wilker Mesquita <wilker@kirschbaumdevelopment.com>
2020-10-29 14:45:31 -0300

    Updating the test to verify morphMany relationship

--- tests/Unit/ChartableTest.php
+++ tests/Unit/ChartableTest.php
@@ -17,9 +17,10 @@ class ChartableTest extends TestCase
         $firstMetricValue = factory(NovaChartjsMetricValue::class)->make();
         $secondMetricValue = factory(NovaChartjsMetricValue::class)->make();
 
-        $this->assertNull($this->testChartable->novaChartjsMetricValue);
+        $this->assertCount(0, $this->testChartable->novaChartjsMetricValue);
         $this->addNovaChartjsMetricValueToChartable($firstMetricValue);
-        $this->assertEquals($firstMetricValue->id, $this->testChartable->fresh()->novaChartjsMetricValue->id);
+        $this->testChartable->fresh();
+        $this->assertNotNull($this->testChartable->novaChartjsMetricValue()->where('chart_name', 'default')->first());
 
         $this->expectException(QueryException::class);
         $this->addNovaChartjsMetricValueToChartable($secondMetricValue);
@@ -29,35 +30,35 @@ class ChartableTest extends TestCase
     {
         $this->addNovaChartjsMetricValueToChartable();
 
-        $this->assertInstanceOf(NovaChartjsMetricValue::class, $this->testChartable->novaChartjsMetricValue);
+        $this->assertInstanceOf(NovaChartjsMetricValue::class, $this->testChartable->novaChartjsMetricValue()->first());
     }
 
     public function testChartableCanAutomaticallyCreateMetricValuesInRelationshipIfPassedBeforeCreating()
     {
         $chartable = new Chartable(['name' => 'Unsaved Chartable']);
-        $testArray = ['January' => 10, 'February' => 30];
+        $testArray = ['chartName' => 'default', 'chartValue' => ['January' => 10, 'February' => 30]];
         $chartable->novaChartjsMetricValue = $testArray;
 
-        $this->assertNull($chartable->novaChartjsMetricValue);
+        $this->assertCount(0, $this->testChartable->novaChartjsMetricValue);
         $chartable->save();
 
         tap($chartable->fresh(), function ($chartable) use ($testArray) {
-            $this->assertInstanceOf(NovaChartjsMetricValue::class, $chartable->novaChartjsMetricValue);
-            $this->assertEquals($testArray, $chartable->novaChartjsMetricValue->metric_values);
+            $this->assertInstanceOf(NovaChartjsMetricValue::class, $chartable->novaChartjsMetricValue()->first());
+            $this->assertEquals($testArray['chartValue'], $chartable->novaChartjsMetricValue()->first()->metric_values);
         });
     }
 
     public function testChartableCanAutomaticallyCreateNewMetricValuesInRelationshipIfNeededAndPassedBeforeUpdating()
     {
-        $this->assertNull($this->testChartable->novaChartjsMetricValue);
+        $this->assertCount(0, $this->testChartable->novaChartjsMetricValue);
 
-        $testArray = ['January' => 10, 'February' => 30];
+        $testArray = ['chartName' => 'default', 'chartValue' => ['January' => 10, 'February' => 30]];
         $this->testChartable->novaChartjsMetricValue = $testArray;
         $this->testChartable->save();
 
         tap($this->testChartable->fresh(), function ($chartable) use ($testArray) {
-            $this->assertInstanceOf(NovaChartjsMetricValue::class, $chartable->novaChartjsMetricValue);
-            $this->assertEquals($testArray, $chartable->novaChartjsMetricValue->metric_values);
+            $this->assertInstanceOf(NovaChartjsMetricValue::class, $chartable->novaChartjsMetricValue()->first());
+            $this->assertEquals($testArray['chartValue'], $chartable->novaChartjsMetricValue()->first()->metric_values);
         });
     }
 
@@ -65,16 +66,45 @@ class ChartableTest extends TestCase
     {
         $metricValue = factory(NovaChartjsMetricValue::class)->make();
         $this->addNovaChartjsMetricValueToChartable($metricValue);
-        $this->assertInstanceOf(NovaChartjsMetricValue::class, $this->testChartable->novaChartjsMetricValue);
-        $this->assertEquals($metricValue->metric_values, $this->testChartable->novaChartjsMetricValue->metric_values);
+        $this->assertInstanceOf(NovaChartjsMetricValue::class, $this->testChartable->novaChartjsMetricValue()->first());
+        $this->assertEquals($metricValue->metric_values, $this->testChartable->novaChartjsMetricValue()->first()->metric_values);
 
-        $testArray = ['January' => 10, 'February' => 30];
+        $testArray = ['chartName' => 'default', 'chartValue' => ['January' => 10, 'February' => 30]];
         $this->testChartable->novaChartjsMetricValue = $testArray;
         $this->testChartable->save();
 
         tap($this->testChartable->fresh(), function ($chartable) use ($testArray, $metricValue) {
-            $this->assertNotEquals($metricValue->metric_values, $chartable->novaChartjsMetricValue->metric_values);
-            $this->assertEquals($testArray, $chartable->novaChartjsMetricValue->metric_values);
+            $this->assertNotEquals($metricValue->metric_values, $chartable->novaChartjsMetricValue()->first()->metric_values);
+            $this->assertEquals($testArray['chartValue'], $chartable->novaChartjsMetricValue()->first()->metric_values);
+        });
+    }
+
+    public function testChartableAdditionOfMoreThanOneMetricValues()
+    {
+        $metricValue = factory(NovaChartjsMetricValue::class)->make();
+        $this->addNovaChartjsMetricValueToChartable($metricValue);
+        $secondaryMetricValue = factory(NovaChartjsMetricValue::class)->make(['chart_name' => 'second']);
+        $this->addNovaChartjsMetricValueToChartable($secondaryMetricValue);
+
+        $this->assertCount(2, $this->testChartable->novaChartjsMetricValue);
+    }
+
+    public function testChartableCanUpdateTheRightInstaceOfMetricValues()
+    {
+        $metricValue = factory(NovaChartjsMetricValue::class)->make();
+        $this->addNovaChartjsMetricValueToChartable($metricValue);
+        $secondaryMetricValue = factory(NovaChartjsMetricValue::class)->make(['chart_name' => 'second']);
+        $this->addNovaChartjsMetricValueToChartable($secondaryMetricValue);
+
+        $this->assertCount(2, $this->testChartable->novaChartjsMetricValue);
+
+        $testArray = ['chartName' => 'second', 'chartValue' => ['January' => 10, 'February' => 30]];
+        $this->testChartable->novaChartjsMetricValue = $testArray;
+        $this->testChartable->save();
+
+        tap($this->testChartable->fresh(), function ($chartable) use ($secondaryMetricValue, $testArray) {
+            $this->assertNotEquals($secondaryMetricValue->metric_values, $chartable->novaChartjsMetricValue()->where('chart_name', 'second')->first()->metric_values);
+            $this->assertEquals($testArray['chartValue'], $chartable->novaChartjsMetricValue()->where('chart_name', 'second')->first()->metric_values);
         });
     }
 
@@ -96,6 +126,6 @@ class ChartableTest extends TestCase
             $chartable = $this->testChartable;
         }
 
-        $chartable->novaChartjsMetricValue()->save($metricValue);
+        $chartable->novaChartjsMetricValue()->create($metricValue->toArray());
     }
 }
--- tests/factories/NovaChartjsMetricValueFactory.php
+++ tests/factories/NovaChartjsMetricValueFactory.php
@@ -5,6 +5,7 @@ use KirschbaumDevelopment\NovaChartjs\Models\NovaChartjsMetricValue;
 
 $factory->define(NovaChartjsMetricValue::class, function (Faker $faker) {
     return [
+        'chart_name' => 'default',
         'metric_values' => [
             'Banana' => $faker->numberBetween(0, 100),
             'Apple' => $faker->numberBetween(0, 100),
