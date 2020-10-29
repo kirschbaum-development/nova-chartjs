aae3928d2ef503f4f6fe878b2468a6f6274e3f5d
Navneet Rai <navneet@kirschbaumdevelopment.com>
2020-10-29 06:34:21 -0400

    Updated contract

--- src/Contracts/Chartable.php
+++ src/Contracts/Chartable.php
@@ -2,14 +2,15 @@
 
 namespace KirschbaumDevelopment\NovaChartjs\Contracts;
 
+use Illuminate\Database\Eloquent\Relations\MorphMany;
 use Illuminate\Database\Eloquent\Relations\MorphOne;
 
 interface Chartable
 {
     /**
-     * @return MorphOne
+     * @return MorphMany
      */
-    public function novaChartjsMetricValue(): MorphOne;
+    public function novaChartjsMetricValue(): MorphMany;
 
     /**
      * Should return settings for Nova Chart in prescribed format.
@@ -21,9 +22,10 @@ interface Chartable
     /**
      * Return a list of all models available for comparison to root model.
      *
+     * @param string $chartName 
      * @return array
      */
-    public static function getNovaChartjsComparisonData(): array;
+     public static function getNovaChartjsComparisonData($chartName = 'default'): array;
 
     /**
      * Return a list of additional datasets added to chart.
