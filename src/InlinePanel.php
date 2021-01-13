aa6585cff920772644e68962acd3f5f41d3b17e3
Krot Eval <38257723+4n70w4@users.noreply.github.com>
2021-01-13 21:18:46 +0300

    continuous-integration/styleci/pr — Issues have been identified with 1 file

--- src/InlinePanel.php
+++ src/InlinePanel.php
@@ -38,6 +38,19 @@ class InlinePanel extends Panel
     }
 
     /**
+     * Specify that the fields should be hidden from the index view.
+     *
+     * @return $this
+     */
+    public function hideFromIndex() {
+        foreach ($this->data as $field) {
+            $field->hideFromIndex();
+        }
+
+        return $this;
+    }
+
+    /**
      * Fields for the inline chart panel.
      *
      * @param Chartable $chartable
@@ -78,18 +91,5 @@ class InlinePanel extends Panel
         $field->chartName($chartName);
 
         return [$field];
-    }
-
-    /**
-     * Specify that the fields should be hidden from the index view.
-     *
-     * @return $this
-     */
-    public function hideFromIndex() {
-        foreach ($this->data as $field) {
-            $field->hideFromIndex();
-        }
-
-        return $this;
     }
 }
